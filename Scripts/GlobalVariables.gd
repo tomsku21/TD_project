c4c027065bd981b190c99edb34513a2f1fe5bd63
Hyndratos <mico.hurskainen@hotmail.com>
2025-05-06 21:29:51 +0300

    Joku hiiri  :D

--- Scripts/GlobalVariables.gd
+++ Scripts/GlobalVariables.gd
@@ -9,3 +9,14 @@ var current_round: int = 0
 var max_rounds: int = 1
 var selected_turret
 var is_mouse_in_Area2D = false
+
+var normal_cursor = load("res://Assets/cursor/normal.png")
+var clicked_cursor = load("res://Assets/cursor/clicked.png")
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed():
+			Input.set_custom_mouse_cursor(clicked_cursor)
+		else:
+			Input.set_custom_mouse_cursor(normal_cursor)
+	
