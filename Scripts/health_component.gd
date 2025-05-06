b4084fafeb99db7d844d66899b146b46601e6037
Hyndratos <mico.hurskainen@hotmail.com>
2025-05-06 16:32:44 +0300

    Poistettu HealthBar.
    Poistin HealthBar noden Enemy  ja turret sceneistä.

--- Scripts/health_component.gd
+++ Scripts/health_component.gd
@@ -10,12 +10,7 @@ var healthbar : ProgressBar
 
 
 func _ready():
-	health = MAX_HEALTH
-	healthbar = %HealthBar
-	healthbar.visible = false
-	healthbar.max_value = MAX_HEALTH
-	healthbar.value = health
-	
+	health = MAX_HEALTH	
 	if get_parent().has_node("AnimatedSprite2D"):
 		sprite = get_parent().get_node("AnimatedSprite2D")
 	elif get_parent().has_node("Sprite2D"):
@@ -29,14 +24,10 @@ func _ready():
 		material.set_shader_parameter("red_tint_amount", 0.0)
 
 func damage(damage):
-	healthbar = %HealthBar
 	health -= damage
-	healthbar.visible = true
-	healthbar.value = health
 	shader_handler()
 	if health <= 0:
 		health = 0
-		healthbar.value = health
 		character.destroy()
 
 
@@ -45,7 +36,7 @@ func shader_handler():
 	var health_ratio = clamp(float(health) / MAX_HEALTH, 0.0, 1.0)
 	material.set_shader_parameter("red_tint_amount", 1.0 - health_ratio)
 	for i in 3:
-		material.set_shader_parameter("flash_amount", 1.0)
+		material.set_shader_parameter("flash_amount", 0.6)
 		await get_tree().create_timer(0.01).timeout
 		material.set_shader_parameter("flash_amount", 0.0)
 		await get_tree().create_timer(0.01).timeout
