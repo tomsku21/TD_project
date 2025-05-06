1909ae4605daff11931a0d64bd4164ec3a83d950
Dalnari <74717729+tomsku21@users.noreply.github.com>
2025-05-06 20:34:36 +0300

    moneystuff
    I swear to god if this causes merge conflicts

--- Scripts/temp_building.gd
+++ Scripts/temp_building.gd
@@ -2,9 +2,11 @@ extends Area2D
 
 var tilemap: TileMapLayer
 var turret: Node
+
 func _ready() -> void:
 	tilemap = get_node("/root/Main/TileMap/Grass")
 	turret = get_node("/root/Main/Turrets")
+	
 func _process(delta):
 	self.global_position = get_global_mouse_position()
 
@@ -14,10 +16,11 @@ func _input(event):
 		var cell = tilemap.local_to_map(tilemap.to_local(world_pos))
 		var tile_data = tilemap.get_cell_tile_data(cell)
 
-		if tile_data and GlobalVariables.is_mouse_in_Area2D == false:
+		if tile_data and GlobalVariables.is_mouse_in_Area2D == false and GlobalVariables.cost >= 1:
 			var tile_type = tile_data.get_custom_data("Testi")
 			
 			if tile_type == true:
+				GlobalVariables.cost -= 1
 				var new_turret = GlobalVariables.selected_turret.instantiate()
 				turret.add_child(new_turret)
 				new_turret.global_position = tilemap.map_to_local(cell)
