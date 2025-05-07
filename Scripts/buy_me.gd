f25455be1f862ae9f7b13d29fdd5356640f4e338
Dalnari <tomsku21@gmail.com>
2025-05-07 10:11:33 +0300

    this shouldn't have take this long.....

--- Scripts/buy_me.gd
+++ Scripts/buy_me.gd
@@ -1,21 +1,18 @@
 extends Panel
 @export var cost: int = 1
-@export var building: PackedScene
 var ghost_node: Node
-@export var item: PackedScene
+
 
 func _ready():
 	ghost_node = get_tree().get_first_node_in_group("Ghost")
 
-func _on_click():
-	print("clicked?")
-	if GlobalVariables.cost >= cost:
-		#instantiate building under cursor, that has code to stay under your pointer.
-		var ghostT = building.instantiate()
-		ghost_node.add_child(ghostT)
-		GlobalVariables.selected_turret = item
-		
-
+func _on_click(button):
+	print(button)
+	#if GlobalVariables.cost >= cost:
+		##instantiate building under cursor, that has code to stay under your pointer.
+		#var ghostT = button.building.instantiate()
+		#ghost_node.add_child(ghostT)
+		#GlobalVariables.selected_turret = button.item
 
 func _on_mouse_entered():
-	pass # Replace with function body.
+	%TDesc.text = %Biyt1.description
