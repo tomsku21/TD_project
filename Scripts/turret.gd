f113580f993c824440176becc6b0b22f9d808d31
Dalnari <tomsku21@gmail.com>
2025-05-07 09:04:42 +0300

    turret upgrade prepping

--- Scripts/turret.gd
+++ Scripts/turret.gd
@@ -4,6 +4,8 @@ extends Area2D
 @onready var healthcomponent: HealthComponent = %HealthComponent
 @export var bullet: PackedScene
 
+var damage_taken: int
+var damage_dealt: int
 
 var sdamage: int = 10 #self damage, to not mix with taken damage from enemies
 var enemies: Array[Node2D] = []
@@ -12,9 +14,8 @@ func _ready() -> void:
 	GlobalVariables.turrets.append(self)
 
 func take_damage(damage: int):
-	#todo:
-	#hit effects here.
 	healthcomponent.damage(damage)
+	damage_taken += damage
 
 
 func destroy():
@@ -39,7 +40,9 @@ func _on_attack_timer_timeout() -> void:
 		if is_instance_valid(target) and target.has_method("take_damage"):
 			#obvs, mut sit instantiatee projektilin, joka suuntaa vastustajan positioon.
 			#projektilille asetettaa instantiates damage, ja homaa/suuntaa vastustajan aikaisempaan positioon.
+			#projektilis also omistaja turretti tietona, kutsuu damagee tehtyään täällä olevaa dealtdmg() funktiota
 			target.take_damage(sdamage)
+			damage_dealt += sdamage
 	
 
 
