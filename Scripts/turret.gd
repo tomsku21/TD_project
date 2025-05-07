32706cd2f82cbab12cac7ac9bc4b7ef0b952b1dd
Hyndratos <mico.hurskainen@hotmail.com>
2025-05-07 17:02:38 +0300

    PEW

--- Scripts/turret.gd
+++ Scripts/turret.gd
@@ -67,6 +67,8 @@ func _on_body_exited(body: Node2D) -> void:
 
 func _on_attack_timer_timeout() -> void:
 	if not enemies.is_empty():
+		$Shoot.pitch_scale = randf_range(0.9, 1.2)
+		$Shoot.play()
 		var target = enemies[0]
 		var new_bullet = bullet.instantiate()
 		add_child(new_bullet)
