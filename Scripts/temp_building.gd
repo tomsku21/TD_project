1e7ef81fa7661400379bb54e7685034069714dc1
Hyndratos <49354762+Hyndratos@users.noreply.github.com>
2025-05-07 09:49:04 +0300

    event system

--- Scripts/temp_building.gd
+++ Scripts/temp_building.gd
@@ -9,9 +9,10 @@ func _ready() -> void:
 	
 func _process(delta):
 	self.global_position = get_global_mouse_position()
+	
 
-func _input(event):
-	if Input.is_action_just_released("click"):
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
 		var world_pos = get_global_mouse_position()
 		var cell = tilemap.local_to_map(tilemap.to_local(world_pos))
 		var tile_data = tilemap.get_cell_tile_data(cell)
@@ -25,4 +26,18 @@ func _input(event):
 				turret.add_child(new_turret)
 				new_turret.global_position = tilemap.map_to_local(cell)
 		queue_free()
+	#if Input.is_action_just_released("click"):
+		#var world_pos = get_global_mouse_position()
+		#var cell = tilemap.local_to_map(tilemap.to_local(world_pos))
+		#var tile_data = tilemap.get_cell_tile_data(cell)
+#
+		#if tile_data and GlobalVariables.is_mouse_in_Area2D == false and GlobalVariables.cost >= 1:
+			#var tile_type = tile_data.get_custom_data("Testi")
+			#
+			#if tile_type == true:
+				#GlobalVariables.cost -= 1
+				#var new_turret = GlobalVariables.selected_turret.instantiate()
+				#turret.add_child(new_turret)
+				#new_turret.global_position = tilemap.map_to_local(cell)
+		#queue_free()
 	
