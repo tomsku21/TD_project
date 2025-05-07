3610161ca898cd68d99d48b63ed98efe74ce12a6
Dalnari <tomsku21@gmail.com>
2025-05-07 12:50:15 +0300

    popupperino

--- Scripts/upgradingButton.gd
+++ Scripts/upgradingButton.gd
@@ -2,3 +2,12 @@ extends TextureButton
 
 @export var new_tower: PackedScene
 @export var description: String
+@export var damage: int
+@export var atk_speed: int
+
+
+func _on_mouse_entered():
+	Popups.setupDescription(self)
+
+func _on_mouse_exited():
+	Popups.returnDesc()
