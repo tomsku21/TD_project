8e9be17cd356cc57a3abb349453639a8ec4ba2c2
Dalnari <tomsku21@gmail.com>
2025-05-08 12:45:24 +0300

    Smol change
    Function will exist in every child code. just the content changes, so function stays in class script

--- Scripts/Tower.gd
+++ Scripts/Tower.gd
@@ -72,16 +72,8 @@ func _on_body_exited(body: Node2D) -> void:
 		if enemies.is_empty():
 			attack_timer.stop()
 
-#func _on_attack_timer_timeout() -> void:
-	#if not enemies.is_empty():
-		#$Shoot.pitch_scale = randf_range(0.9, 1.2)
-		#$Shoot.play()
-		#var target = enemies[0]
-		#var new_bullet = bullet.instantiate()
-		#add_child(new_bullet)
-		#new_bullet.gun = marker_2d
-		#new_bullet.target = target
-		#new_bullet.damage = sdamage
+func _on_attack_timer_timeout() -> void:
+	pass
 	
 
 func upgrade():
