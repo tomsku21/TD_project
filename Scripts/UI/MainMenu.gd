1a1ba6f64c5a3d29e4e12ddc83c430970ad17c56
Dalnari <tomsku21@gmail.com>
2025-05-15 10:37:52 +0300

    second map
    testable

--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@export var gameScene: PackedScene
 var masterIndex: int
 var musicIndex: int
 var sfxIndex: int
@@ -42,7 +43,7 @@ func _on_button_pressed(_name):
 		"Start":
 			#Change later to open stage select panel, if you make multiple layouts, 
 			#otherwise just push player to main scene.
-			get_tree().change_scene_to_file("res://Scenes/Mainbkup.tscn")
+			get_tree().change_scene_to_packed(gameScene)
 			GlobalVariables.in_mainMenu = false
 			GlobalVariables.game_state = true
 		"Options":
