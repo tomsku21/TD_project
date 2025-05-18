b9d329ad497453d9d49d91f842e1229d3c8d28fb
Hyndratos <mico.hurskainen@hotmail.com>
2025-05-18 16:51:30 +0300

    .

--- Scripts/Tower/HealerPlant.gd
+++ Scripts/Tower/HealerPlant.gd
@@ -4,13 +4,9 @@ extends SupportTower
 @export var heal_range: Heal_Range_Component
 
 func _on_attack_timer_timeout() -> void:
-	print("trying to heal")
 	if not heal_range.towers.is_empty():
-		print("there is a healable tower to pump")
 		for i in heal_range.towers.size():
-			print("going through towers")
 			var target = heal_range.towers[i]
 			if target != null:
-				print("pushing out the wave")
 				target.Heal(stats["Healing"], self)
 		Attack_effect.emitting = true
